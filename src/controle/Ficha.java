740c2c8d1aff5af0d86456f73ec106bd71091394
peonx6 <edsonhenrich@gmail.com>
2025-12-21 19:52:51 -0300

    Melhoria classe 'Ficha' e algumas correções

--- src/controle/Ficha.java
+++ src/controle/Ficha.java
@@ -5,17 +5,38 @@ import java.util.ArrayList;
 
 public class Ficha {
     private ArrayList<Exercicio> exercicios;
+    private String dataCreate;
+    private String ultimaAtualizacao;
     
     public Ficha() {
         this.exercicios = new ArrayList<>();
+        this.dataCreate = java.time.LocalDate.now().toString();
+        this.ultimaAtualizacao = java.time.LocalDate.now().toString();
+    }
+
+    public Ficha(String dataCreate) {
+        this.exercicios = new ArrayList<>();
+        this.dataCreate = dataCreate;
+        this.ultimaAtualizacao = java.time.LocalDate.now().toString();
     }
     
     public void adicionarExercicio(Exercicio exercicio) {
-        exercicios.add(exercicio);
+        if (exercicio != null) {
+            exercicios.add(exercicio);
+            atualizarData();
+        }
     }
     
     public void removerExercicio(Exercicio exercicio) {
         exercicios.remove(exercicio);
+        atualizarData();
+    }
+
+    public void removerExercicioPorIndice(int indice) {
+        if (indice >= 0 && indice < exercicios.size()) {
+            exercicios.remove(indice);
+            atualizarData();
+        }
     }
     
     public ArrayList<Exercicio> getExercicios() {
@@ -24,5 +45,43 @@ public class Ficha {
     
     public void setExercicios(ArrayList<Exercicio> exercicios) {
         this.exercicios = exercicios;
+        atualizarData();
+    }
+
+    public int totalExercicios() {
+        return exercicios.size();
+    }
+
+    public Exercicio pesquisarExercicioPorNome(String nome) {
+        return exercicios.stream()
+                .filter(e -> e.getNome().toLowerCase().contains(nome.toLowerCase()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public boolean contemExercicio(String nomeExercicio) {
+        return exercicios.stream()
+                .anyMatch(e -> e.getNome().equalsIgnoreCase(nomeExercicio));
+    }
+
+    private void atualizarData() {
+        this.ultimaAtualizacao = java.time.LocalDate.now().toString();
+    }
+
+    public String getDataCreate() {
+        return dataCreate;
+    }
+
+    public String getUltimaAtualizacao() {
+        return ultimaAtualizacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha{" +
+                "totalExercicios=" + exercicios.size() +
+                ", dataCriacao='" + dataCreate + '\'' +
+                ", ultimaAtualizacao='" + ultimaAtualizacao + '\'' +
+                '}';
     }
 }
--- src/principal/Exercicio.java
+++ src/principal/Exercicio.java
@@ -2,5 +2,13 @@ package principal;
 
 public abstract class Exercicio {
     protected String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
 
